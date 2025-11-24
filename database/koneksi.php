af5d91e69182ee273777c75ced47e4a71639dcb5
Valentinoo <valentinoo7229@gmail.com>
2025-11-24 11:36:40 +0700

    Add files via upload

--- database/koneksi.php
+++ database/koneksi.php
@@ -1,1 +1,12 @@
+<?php
+ $host = "localhost";
+ $user = "root"; // Ganti dengan username database Anda
+ $pass = ""; // Ganti dengan password database Anda
+ $db = "db_kasir"; // Nama database yang Anda buat
 
+ $koneksi = mysqli_connect($host, $user, $pass, $db);
+
+if (!$koneksi) {
+    die("Koneksi gagal: " . mysqli_connect_error());
+}
+?>
